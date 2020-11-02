509d27aaba323bd0ac03c66bf8b287b08e04c133
chaaruPradeep <38594711+chaaruPradeep@users.noreply.github.com>
2020-11-02 14:01:16 -0500

    Rental App

---
+++ dbo.AddCase.data.sql
@@ -0,0 +1,14 @@
+﻿SET IDENTITY_INSERT [dbo].[AddCase] ON
+INSERT INTO [dbo].[AddCase] ([CaseId], [CaseTitle], [CaseDescription], [LoginId]) VALUES (4, N'charuma', N'hsdgfhjgs', 5)
+INSERT INTO [dbo].[AddCase] ([CaseId], [CaseTitle], [CaseDescription], [LoginId]) VALUES (11, N'sfdgmk', N'ryty', 6)
+INSERT INTO [dbo].[AddCase] ([CaseId], [CaseTitle], [CaseDescription], [LoginId]) VALUES (1013, N'sfdg', N'ryty', 7)
+INSERT INTO [dbo].[AddCase] ([CaseId], [CaseTitle], [CaseDescription], [LoginId]) VALUES (1014, N'cd', N'cd', 4)
+INSERT INTO [dbo].[AddCase] ([CaseId], [CaseTitle], [CaseDescription], [LoginId]) VALUES (1015, N'bgm', N'sdgfgh', 5)
+INSERT INTO [dbo].[AddCase] ([CaseId], [CaseTitle], [CaseDescription], [LoginId]) VALUES (1016, N'abc', N'rytyty', 6)
+INSERT INTO [dbo].[AddCase] ([CaseId], [CaseTitle], [CaseDescription], [LoginId]) VALUES (1017, N'nithin', N'th', 7)
+INSERT INTO [dbo].[AddCase] ([CaseId], [CaseTitle], [CaseDescription], [LoginId]) VALUES (2017, N'ni', N'ghggjhhm', 4)
+INSERT INTO [dbo].[AddCase] ([CaseId], [CaseTitle], [CaseDescription], [LoginId]) VALUES (2018, N'reg', N'fgh', 5)
+INSERT INTO [dbo].[AddCase] ([CaseId], [CaseTitle], [CaseDescription], [LoginId]) VALUES (2019, N'gfdd', N'jgghfhgf', 5)
+INSERT INTO [dbo].[AddCase] ([CaseId], [CaseTitle], [CaseDescription], [LoginId]) VALUES (2020, N'hghgf', N'bcbvcbvn', 4)
+INSERT INTO [dbo].[AddCase] ([CaseId], [CaseTitle], [CaseDescription], [LoginId]) VALUES (2021, N'hghg', N'ncvc', 6)
+SET IDENTITY_INSERT [dbo].[AddCase] OFF
